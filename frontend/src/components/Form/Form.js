cb824916e9fdb5fe6067b16b222ec8c7ddb80cb1
edsonpolucena <edsonpolucena@hotmail.com>
2024-04-14 16:57:22 -0300

    feat: Estilizado formulário de receita:
    Adicionadas cores, espaçamentos e estilos aos campos e botões do formulário de adição de receita.

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
 import { useGlobalContext } from '../../context/globalContext';
+import Button from '../Button/Button';
+import {plus} from '../../utils/icons';
 
 function Form() {
-    const {addIncome} = useGlobalContext()
+    const {addIncome, getIncomes} = useGlobalContext()
     const [inputState, setInputState] = useState({
         title:'',
         amount:'',
@@ -22,6 +24,13 @@ function Form() {
     const handleSubmit = e => {
         e.preventDefault()
         addIncome(inputState)
+        setInputState({
+            title:'',
+            amount:'',
+            date:'',
+            category:'',
+            description:'',  
+        })
     }
     return (
         <FormStyled onSubmit={handleSubmit}>
@@ -29,20 +38,20 @@ function Form() {
                 <input type="text"
                 value={title}
                 name={'title'}
-                placeholder="Salary Title"
+                placeholder="Título Renda"
                 onChange={handleInput('title')}/>
             </div>
             <div className="input-control">
                 <input value={amount}
                 type="text"
                 name={'amount'}
-                placeholder="Salary Amount"
+                placeholder="Valor Renda"
                 onChange={handleInput('amount')}/>
             </div>
             <div className="input-control">
                 <DatePicker
                     id='date'
-                    placeholderText='Enter A Date'
+                    placeholderText='Inserir Data'
                     selected={date}
                     dateFormat="dd/MM/yyyy"
                     onChange={(date)=>{
@@ -54,30 +63,81 @@ function Form() {
             </div>
             <div className="selects input-control">
                 <select required value={category} name="category" id="category" onChange={handleInput('category')}>
-                   <option value="" disable>Select Option</option>
-                   <option value="salary" disable>Salary</option> 
-                   <option value="freelancing" disable>Freelancing</option> 
-                   <option value="investiments" disable>Investiments</option> 
-                   <option value="stocks" disable>Stocks</option> 
+                   <option value="" disable>Selecione Tipo</option>
+                   <option value="salary" disable>Salário</option> 
+                   <option value="freelancing" disable>Freelance</option> 
+                   <option value="investments" disable>Investimento</option> 
+                   <option value="stocks" disable>Ações</option> 
                    <option value="bitcoin" disable>Bitcoin</option> 
-                   <option value="bank" disable>Bank Transfer</option> 
+                   <option value="bank" disable>Transf Bancária</option> 
                    <option value="youtube" disable>Youtube</option> 
-                   <option value="other" disable>Other</option> 
+                   <option value="other" disable>Outros</option> 
 
                 </select>
             </div>
             <div className="input-control">
-                <textarea name="description" value={description} placeholder='Ass a Reference' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
+                <textarea name="description" value={description} placeholder='Comentário' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
             </div>
             <div className="submit-btn">
-                <button>ADD Income</button>
+                <Button
+                    name={'Adicione'}
+                    icon={plus}
+                    bPad={'.8rem 1.6rem'}
+                    bRad={'30px'}
+                    bg={'var(--color-accent)'}
+                    color={'#fff'}
+                    hcolor={'red'}
+                />
             </div>
         </FormStyled>
 )
 }
 
 const FormStyled = styled.form`
-
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    input, textarea, select{
+        font-family: inherit;
+        font-size: inherit;
+        outline: none;
+        border: none;
+        padding: .5rem 1rem;
+        border-radius: 5px;
+        border: 2px solid #fff;
+        background: transparent;
+        resize: none;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        color: rgba(34, 34, 96, 0.9);
+        &::placeholder{
+            rgba(34, 34, 96, 0.4);
+        }
+       
+    }
+    .input-control{
+        input{
+            width: 100%;
+        }
+    }
+    .selects{
+        display: flex;
+        justify-content: flex-end;
+        select{
+            color: rgba(34, 34, 96, 0.4);
+            &:focus, &:active{
+                color: rgba(34, 34, 96, 1);
+            }
+        }
+    }
+    .submit-btn{
+        button{
+            box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+            &:hover{
+                background: var(--color-green) !important;
+            }
+        }
+    }
 
 `;
 export default Form
--- frontend/src/components/Income/Income.js
+++ frontend/src/components/Income/Income.js
@@ -1,20 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import {InnerLayout} from '../../styles/layouts';
 import { useGlobalContext } from '../../context/globalContext';
 import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Income() {
-  const {addIncome} = useGlobalContext()
+  const {addIncome, incomes, getIncomes, deleteIncome, totalIncome} = useGlobalContext()
+
+  useEffect(() =>{
+    getIncomes()
+
+  }, [incomes])
   return (
     <IncomeStyled>
         <InnerLayout>
         <h1>Receitas</h1>
+        <h2 className="total-income">Total Receita: <span>${`${totalIncome()}`}</span> </h2>
+
         <div className="income-content">
           <div className="form-container">
             <Form/>
           </div>
           <div className="incomes">
+            {incomes.map((income)=>{
+              const {_id, title, amount, date, category, description, type} = income;
+              return <IncomeItem
+                  key={_id}
+                  id={_id}
+                  title={title}
+                  description={description}
+                  amount={amount}
+                  date={date}
+                  type={type}
+                  category={category}
+                  indicatorColor="var(--color-green)"
+                  deleteItem={deleteIncome}
+              
+              />
+            })}
 
           </div>
         </div>
@@ -23,7 +47,36 @@ function Income() {
   );
 }
 
-const IncomeStyled = styled.div``;
+const IncomeStyled = styled.div`
+  display: flex;
+  overflow: auto;
+  .total-income{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fcf6f9;
+    border: 2px solid #ffffff;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    border-radius: 20px;
+    padding: 1rem;
+    margin: 1rem 0;
+    font-size: 2rem;
+    gap: .5rem;
+    span{
+      font-size: 2.5rem;
+      font-weight: 800;
+      color: var(--color-green);
+    }
+
+  }
+  .income-content{
+    display: flex;
+    gap: 1rem;
+    .incomes{
+      flex: 1;
+    }
+  }
+`;
 
 
 
